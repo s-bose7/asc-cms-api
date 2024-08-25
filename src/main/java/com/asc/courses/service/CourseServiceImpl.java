268e0a3f69776e0782d7b29ed3a91b0bccd55ffc
Sudip Bose <sbose007ime.work@gmail.com>
2024-08-25 19:53:11 +0530

    Enhancement: 'saveCourse' Validation and Course Model Constraints
    Added input validation using the @Valid annotation in the saveCourse controller to ensure that only valid course data is accepted
    Updated Course model to include constraints for course attribute sizes (@Size, @NotBlank), ensuring data integrity at the model level.
    Implemented Regex-based validation in the service layer to identify and prevent the creation of courses with invalid course codes.

--- src/main/java/com/asc/courses/service/CourseServiceImpl.java
+++ src/main/java/com/asc/courses/service/CourseServiceImpl.java
@@ -2,12 +2,15 @@ package com.asc.courses.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.asc.courses.model.Course;
 import com.asc.courses.exceptions.CourseExistException;
+import com.asc.courses.exceptions.InvalidCourseException;
 import com.asc.courses.repository.CourseRepository;
 
 
@@ -20,21 +23,31 @@ public class CourseServiceImpl implements CourseService {
         this.courseRepository = courseRepository;
     }
 
+    private boolean isNotValidCourseCode(String code) {
+        // Regex to match any character that is NOT a letter (a-z, A-Z),
+        // a digit (0-9), a period (.), or a space (\u0020). 
+        String regex = "[^a-zA-Z0-9. ]";
+        Matcher matcher = Pattern.compile(regex).matcher(code);
+        // returns true if the course code contains any character 
+        // that matches the regex (i.e., any invalid character)
+        return matcher.find();
+    }
 
     @Override
-    public Course saveCourse(Course course) throws CourseExistException {
-        Course newCourse;
+    public Course saveCourse(Course course) throws CourseExistException, InvalidCourseException {
+        if(isNotValidCourseCode(course.getCourseCode())){
+            throw new InvalidCourseException("Course code is not valid: "+course.getCourseCode());
+        }
         try {
             Optional<Course> savedCourse = fetchCourseByCode(course.getCourseCode());
             if(savedCourse.isPresent()){
                 throw new CourseExistException("Course already present with code: "+course.getCourseCode());
             }
-            newCourse = courseRepository.save(course);
+            return courseRepository.save(course);
 
         } catch (RuntimeException e) { 
             throw new RuntimeException("Failed to save course: " + course.getCourseTitle());
         }
-        return newCourse;
     }
 
 
