c5441196c7b0cb04bd68c8649c0d231b28a40277
Sudip Bose <sbose007ime.work@gmail.com>
2024-08-27 20:41:40 +0530

    Add: Sample dataset for tests and migration commands

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXIST courses (
+    id bigint not null auto_increment,
+    course_code varchar(10) not null,
+    course_title varchar(60) not null,
+    course_description varchar(300) not null,
+    primary key (id)
+)
+
+CREATE TABLE IF NOT EXIST session (
+    id bigint not null auto_increment,
+    course_id bigint not null,
+    year_of_delivery integer not null,
+    semester_of_delivery integer not null,
+    primary key (id)
+) 
+
+ALTER TABLE courses 
+    ADD CONSTRAINT UKp02ts69sh53ptd62m3c67v0 
+    UNIQUE (course_code)
+
+ALTER TABLE session 
+    ADD CONSTRAINT FKde01hisfl6rwwskjaslge4kj2 
+    FOREIGN KEY (course_id) 
+    REFERENCES courses (id) 
+    ON DELETE CASCADE
