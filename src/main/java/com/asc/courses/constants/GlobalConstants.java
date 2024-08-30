cbadf8257d99a93f0ba9ffbd7313fc35475701dd
Sudip Bose <sbose007ime.work@gmail.com>
2024-08-30 21:24:47 +0530

    Add: Global constants for storing error codes

--- src/main/java/com/asc/courses/constants/GlobalConstants.java
+++ src/main/java/com/asc/courses/constants/GlobalConstants.java
@@ -2,4 +2,8 @@ package com.asc.courses.constants;
 
 public class GlobalConstants {
     
+    public static final String ERR_BAD_REQUEST = "ERR_BAD_REQUEST";
+    public static final String ERR_CONFLICT = "ERR_CONFLICT";
+    public static final String ERR_INTERNAL_SERVER = "ERR_INTERNAL_SERVER";
+    public static final String ERR_NOT_FOUND = "ERR_NOT_FOUND";
 }
